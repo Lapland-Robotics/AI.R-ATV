95e0ea651df2570467453c3a4e84db5c7cba29c7
Sohan Janaka <sohanjanaka101@gmail.com>
2024-05-27 20:31:28 +0300

    init

---
+++ ros2_ws/src/esp32_microros_controller/esp32_microros_controller/esp32_microros_controller.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+import rclpy
+from rclpy.executors import ExternalShutdownException
+from rclpy.node import Node
+from std_msgs.msg import Int32
+
+
+class Esp32ControllerNode(Node):
+
+    def __init__(self):
+        super().__init__('esp32_node')
+        topic = "/esp32"
+        self.get_logger().info('Topic = ' + topic)
+        self.sub = self.create_subscription(Int32, topic, self.chatter_callback, 10)
+
+    def chatter_callback(self, msg: Int32):
+        self.get_logger().info(str(msg))
+
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = Esp32ControllerNode()
+    try:
+        rclpy.spin(node)
+    except (KeyboardInterrupt, ExternalShutdownException):
+        pass
+    finally:
+        node.destroy_node()
+        rclpy.try_shutdown()
+
+
+if __name__ == '__main__':
+    main()
---
+++ ros_entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+
+source /opt/ros/$ROS_DISTRO/setup.bash
+source /app/ros2_ws/install/setup.bash
+
+source /uros_ws/install/local_setup.sh
+
+ros2 run micro_ros_agent micro_ros_agent udp4 --port 8888 &
+
+exec "$@"
