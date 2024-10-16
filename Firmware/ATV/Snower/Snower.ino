60482a21f6ce7ae90cc295400ca1b0a48a83e0ce
Sohan Janaka <sohanjanaka101@gmail.com>
2024-10-16 12:40:13 +0300

    US_10388: Teensy -> ESP32

--- Firmware/ATV/Snower/Snower.ino
+++ Firmware/ATV/Snower/Snower.ino
@@ -1,16 +1,16 @@
 //Pins
-#define DIG1  22 //Direction ch1
-#define DIG2  23 //Direction ch2
 #define CH1   18 //Throttle ch1
 #define CH2   19 //Throttle ch2
-#define AOUT1 14 //Output ch1
-#define AOUT2 15 //Output ch2
+#define DIG1  22 //Direction ch1
+#define DIG2  23 //Direction ch2
+#define AOUT1 25 //Output ch1
+#define AOUT2 26 //Output ch2
  
 //Constants
 #define FREQ  490  //AnalogWrite frequency
 #define MAX_T 2500 //Max signal threshold
 #define MIN_T 500  //Min signal threshold
-
+#define RESOLUTION 8 //PWM resolution (8-bit, range from 0-255)
 
 float turnFactor = 0.5; 
 //Variables
@@ -26,9 +26,12 @@ void setup() {
   pinMode(DIG1, OUTPUT);
   pinMode(DIG2, OUTPUT);
  
-  //Initialising analog outputs
-  analogWriteFrequency(14, FREQ);
-  analogWriteFrequency(15, FREQ);
+  //Initialising PWM on ESP32
+  ledcSetup(0, FREQ, RESOLUTION); // Channel 0 for AOUT1
+  ledcSetup(1, FREQ, RESOLUTION); // Channel 1 for AOUT2
+  
+  ledcAttachPin(AOUT1, 0); // Attach AOUT1 to channel 0
+  ledcAttachPin(AOUT2, 1); // Attach AOUT2 to channel 1
  
   //initialising digital inputs
   pinMode(CH1, INPUT);
@@ -42,10 +45,7 @@ void loop() {
   //X-axis control
   if(xRaw > MIN_T && xRaw < MAX_T && yRaw > MIN_T && yRaw < MAX_T)
   { 
-    //Serial.print("X-Raw:");
-    //Serial.println(xRaw);
-    //Serial.print("Y-Raw:");
-    //Serial.println(yRaw);
+
     xAxis = map(xRaw,993,2016,-255,255);
     yAxis = map(yRaw,1027,2010,-255,255);
     
@@ -53,18 +53,19 @@ void loop() {
 
     
     digitalWrite(DIG1, motor1 >= 0);
-    analogWrite(AOUT1, abs(motor1));
+    ledcWrite(0, abs(motor1)); // Writing PWM to channel 0 (AOUT1)
     digitalWrite(DIG2, motor2 >= 0);
-    analogWrite(AOUT2, abs(motor2));
+    ledcWrite(1, abs(motor2)); // Writing PWM to channel 1 (AOUT2)
  
-    Serial.print("xAxis:");
-    Serial.println(xAxis);
-    Serial.print("yAxis:");
-    Serial.println(yAxis);
-    Serial.print("Motor1:");
-    Serial.println(motor1);
-    Serial.print("Motor2:");
+    Serial.print("xAxis: ");
+    Serial.print(xAxis);
+    Serial.print(", yAxis: ");
+    Serial.print(yAxis);
+    Serial.print(", Motor1: ");
+    Serial.print(motor1);
+    Serial.print(", Motor2: ");
     Serial.println(motor2);
+    delay(50);
   }
   
   else {
@@ -72,7 +73,7 @@ void loop() {
     {
       Serial.println("CHANNEL X DEAD");
       digitalWrite(DIG1, HIGH);
-      analogWrite(AOUT1, 0);
+      ledcWrite(0, 0); // Stop PWM output on channel 0
     }
     else {
       Serial.println("AXIS CONTROL MAP ERROR");
@@ -82,7 +83,7 @@ void loop() {
     {
     Serial.println("CHANNEL Y DEAD");
     digitalWrite(DIG2, HIGH);
-    analogWrite(AOUT2, 0);
+    ledcWrite(1, 0); // Stop PWM output on channel 1
     }
     else {
       Serial.println("AXIS CONTROL MAP ERROR");
