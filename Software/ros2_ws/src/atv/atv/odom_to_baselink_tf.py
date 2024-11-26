8b17771948a458837ca206a0e4cb752ca56e251f
Sohan Janaka <sohanjanaka101@gmail.com>
2024-11-26 17:58:11 +0200

    US_10825: urdf, map -> odom and odom -> base_link tf

---
+++ Software/ros2_ws/src/atv/atv/odom_to_baselink_tf.py
@@ -0,0 +1,39 @@
+import rclpy
+from rclpy.node import Node
+from tf2_ros import TransformBroadcaster
+from geometry_msgs.msg import TransformStamped
+from nav_msgs.msg import Odometry
+
+class OdomToBaseLinkTF(Node):
+    def __init__(self):
+        super().__init__('odom_to_base_link_broadcaster')
+
+        # Initialize the Transform Broadcaster
+        self.tf_broadcaster = TransformBroadcaster(self)
+
+        # Subscribe to the /odom topic
+        self.subscription = self.create_subscription(Odometry, '/odom', self.handle_odom, 10)
+    
+    def handle_odom(self, msg: Odometry):
+        
+        t = TransformStamped()
+        t.header.stamp = self.get_clock().now().to_msg()
+        t.header.frame_id = 'odom'
+        t.child_frame_id = 'base_link'
+        t.transform.translation.x = msg.pose.pose.position.x
+        t.transform.translation.y = msg.pose.pose.position.y
+        t.transform.translation.z = msg.pose.pose.position.z
+        t.transform.rotation = msg.pose.pose.orientation
+
+        # Broadcast the transform
+        self.tf_broadcaster.sendTransform(t)
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = OdomToBaseLinkTF()
+    rclpy.spin(node)
+    node.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
--- Software/ros2_ws/src/atv/launch/run.launch.py
+++ Software/ros2_ws/src/atv/launch/run.launch.py
@@ -3,17 +3,40 @@ from launch import LaunchDescription
 from launch_ros.actions import Node
 
 def generate_launch_description():
+    urdf_path = 'ros2_ws/src/atv/urdf/snower.urdf'
+    with open(urdf_path, 'r') as urdf_file:
+        robot_description = urdf_file.read()
+        
     return LaunchDescription([
-        Node(
-            package='atv',
-            executable='atv',
-            name='atv',
-            output='screen'
-        ),
+        # Node(
+        #     package='atv',
+        #     executable='atv',
+        #     name='atv',
+        #     output='screen'
+        # ),
         # Node(
         #     package='atv',
         #     executable='data_collector',
         #     name='data_collector',
         #     output='screen'
-        # )
+        # ),
+        Node(
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            name='robot_state_publisher',
+            output='screen',
+            parameters=[{'robot_description': robot_description}]
+        ),
+        Node(
+            package='atv',
+            executable='odom_to_baselink_tf',
+            name='odom_to_baselink_tf',
+            output='screen'
+        ),
+        Node(
+            package='atv',
+            executable='map_to_odom_tf',
+            name='odom_to_baselink_tf',
+            output='screen'
+        ),
     ])
