1ed9829b6b4d6f815d8d3495011c949b80254ffe
Sohan Janaka <sohanjanaka101@gmail.com>
2025-03-05 16:20:51 +0200

    US_10512: wireless trigger switch error loop

--- Firmware/Trigger/Trigger.ino
+++ Firmware/Trigger/Trigger.ino
@@ -15,24 +15,39 @@ rcl_node_t node;
 int lastState = HIGH;
 int currentState;
 
-#define RCCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){}}
+#define RCCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){errorLoop();}}
 #define RCSOFTCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){}}
 #define BUTTON_PIN 21 
 
-void setup() {
-  
-  set_microros_wifi_transports("Sohan_A54", "12345678", "192.168.102.38", 8888); // microros over wifi
-  // set_microros_transports(); // microros over serial
+void microrosInit(){
+  set_microros_transports(); // microros over serial
   allocator = rcl_get_default_allocator();
   RCCHECK(rclc_support_init(&support, 0, NULL, &allocator));
   RCCHECK(rclc_node_init_default(&node, "micro_ros_esp32_node", "", &support));
   RCCHECK(rclc_publisher_init_best_effort(&publisher, &node, ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Bool), "/trigger"));
 
   msg.data = false;
-
-  pinMode(BUTTON_PIN, INPUT_PULLUP);
-
 }
+
+void setup() {
+  microrosInit();
+  pinMode(BUTTON_PIN, INPUT_PULLUP);
+}
+
+void errorLoop() {
+    static int retryCount = 0;
+    retryCount++;
+    delay(2000);
+
+    if (retryCount > 5) {
+      Serial.println("[ERROR LOOP] Too many retries, halting or resetting system...");
+      // force a system reset and try again
+      ESP.restart(); 
+    } 
+    else {
+      microrosInit();
+    }
+  }  
 
 void loop() {
 
