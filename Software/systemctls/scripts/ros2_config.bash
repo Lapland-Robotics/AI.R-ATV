b29d853eaee3dff6114ef5c8372ef85874df6677
Sohan Janaka <sohanjanaka101@gmail.com>
2025-05-13 10:26:40 +0300

    US_12222: cyclone dds xml

--- Software/systemctls/scripts/ros2_config.bash
+++ Software/systemctls/scripts/ros2_config.bash
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 export RMW_IMPLEMENTATION=rmw_cyclonedds_cpp
+export CYCLONEDDS_URI="file://home/robotics/repo/AI.R-ATV/Software/cyclonedds-config.xml"
 
 source /opt/ros/humble/setup.bash
 
