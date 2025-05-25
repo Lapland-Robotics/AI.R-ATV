0c808252c31f175c8d6ce369a7bcf55a46e5b16e
Sohan Janaka <sohanjanaka101@gmail.com>
2025-05-25 21:59:49 +0300

    US_12426: init zenoh config

--- Software/systemctls/scripts/ros2_config.bash
+++ Software/systemctls/scripts/ros2_config.bash
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-export RMW_IMPLEMENTATION=rmw_cyclonedds_cpp
-export CYCLONEDDS_URI=file:///home/robotics/repo/AI.R-ATV/Software/cyclonedds-config.xml
+# export RMW_IMPLEMENTATION=rmw_cyclonedds_cpp
+# export CYCLONEDDS_URI=file:///home/sohan-lapinamk/repos/Autonomous_Robot/Software/cyclonedds-config.xml
+
+#export RMW_IMPLEMENTATION=rmw_zenoh_cpp
+#export ZENOH_ROUTER_CONFIG_URI=/home/sohan-lapinamk/repos/Autonomous_Robot/Software/zenoh-router-config.json5
+
 
 source /opt/ros/humble/setup.bash
